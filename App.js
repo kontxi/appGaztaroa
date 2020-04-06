58f888a2ee5c973ec7f8539a4b14a78cb85251d8
Kontxi Aginaga <kontxiisabel.aginaga@unavarra.es>
2020-04-06 11:37:09 +0200

    Componentes React Native

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Campobase />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
